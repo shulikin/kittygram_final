a1b47d01e5fedf97fc1ff17e88c08b971e8c9ba7
shulikin <alex@shulikin.com>
2024-09-18 13:06:47 +0400

    God_help_me

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,6 @@ PORT_DB = 5432
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 IP = os.getenv('IP')
 DOMAIN = os.getenv('DOMAIN')
